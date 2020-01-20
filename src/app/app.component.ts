7127b805925514d9a08cbe30406c943388c4a63b
iromashko <iromashko@me.com>
2020-01-20 14:48:27 +0300

    add event handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = "";
+
+  onButtonClick() {
+    this.password = "YOUR PASSWORD";
+  }
+  onChangeUseLetters() {
+    console.log(this.includeLetters);
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    console.log(this.includeNumbers);
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    console.log(this.includeSymbols);
+    this.includeSymbols = !this.includeSymbols;
+  }
 }
