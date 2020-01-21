d9c08398c25817c520986d69d875506f78ab2e5e
iromashko <iromashko@me.com>
2020-01-21 08:25:18 +0300

    add generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,45 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   password = "";
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
-  length = 0;
   onButtonClick() {
-    console.log(`
-      Generate password:
-      Letters: ${this.includeLetters}
-      Numbers: ${this.includeNumbers}
-      Symbols: ${this.includeSymbols}
-    `);
-    this.password = "my password";
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!#$#$%(#*%)^";
+
+    let validChars = "";
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    let generatedPassword = "";
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
-  onChangeIncludeLetters() {
+  onIncludeLettersChange() {
     this.includeLetters = !this.includeLetters;
   }
-  onChangeIncludeNumbers() {
+  onIncludeNumbersChange() {
     this.includeNumbers = !this.includeNumbers;
   }
-  onChangeIncludeSymbols() {
+  onIncludeSymbolsChange() {
     this.includeSymbols = !this.includeSymbols;
   }
   onChangeLength(value: string) {
-    const parsedValue = parseInt(value, 10);
-    if (!isNaN(parsedValue)) {
-      this.length = parsedValue;
+    const parsedLength = parseInt(value, 10);
+    if (!isNaN(parsedLength)) {
+      this.length = parsedLength;
     }
   }
 }
